494ea8d6b7e6abc8106c503749b2f38464491787
KristofferK <kristoffer@minlejebolig.dk>
2018-05-05 23:45:30 +0200

    Fix linting errors

--- thirstyboy/src/main.ts
+++ thirstyboy/src/main.ts
@@ -16,12 +16,13 @@ const credentails = credentialsSaver.restore();
 const updateDiv = (div: HTMLDivElement) => (message: string, className: string) => {
   div.innerText = message;
   div.className = className;
-}
+};
+
 const statusBar = <HTMLDivElement>document.getElementById('status-bar');
 const updateStatusBar = updateDiv(statusBar);
 
-const runElem = <HTMLButtonElement>document.querySelector("#perform-login");
-runElem.addEventListener('click', (e: MouseEvent) => {
+const loginButton = <HTMLButtonElement>document.querySelector("#perform-login");
+loginButton.addEventListener('click', (e: MouseEvent) => {
   const form = new FormData(document.querySelector('form'))
   const email = form.get('email').toString();
   const password = form.get('password').toString();
@@ -41,4 +42,4 @@ runElem.addEventListener('click', (e: MouseEvent) => {
       updateStatusBar('Sucessfully signed in.', 'bg-success');
     }
   })
-})
+});
